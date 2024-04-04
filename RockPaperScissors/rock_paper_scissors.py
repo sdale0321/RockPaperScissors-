7e8c576c0f9c9fbe454abf2bb6c472ace5981807
sdale0321 <67200326+sdale0321@users.noreply.github.com>
2024-04-03 22:19:48 -0500

    Added notes, covering concepts

--- RockPaperScissors/rock_paper_scissors.py
+++ RockPaperScissors/rock_paper_scissors.py
@@ -1,39 +1,43 @@
 import random 
 
-user_wins = 0
-computer_wins= 0
+user_wins = 0 #rock, paper, scissors is a 2 player game
+computer_wins= 0 #creating 2 variables to account for each score 
 
-options = ["rock", "paper", "scissors"]
+options = ["rock", "paper", "scissors"]#variable to account for all options in the game 
 
 while True:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower() #making things case insesitive 
     if user_input == "q":
-        break
+        break #enables user to stop progam whenever they choose 
 
     if user_input not in ["rock", "paper", "scissors"]:
-        continue #recursive 
+        continue #recursive, starts the loop over asking them to type again 
 
-    random_number = random.randint(0,2) 
+    random_number = random.randint(0,2) #generating random number b/w 0 and 2 
     #rock is 0, paper is 1, and scissors is 2 
-    computer_pick = options[random_number]
+    computer_pick = options[random_number] #
     print("Computer picked", computer_pick + ".")
 
-    if user_input == "rock" and computer_pick == "scissors":
+    if user_input == "rock" and computer_pick == "scissors": #checking if user won based on computer pick 
         print("You won!")
-        user_wins += 1
+        user_wins += 1 
         continue
     elif user_input == "paper" and computer_pick == "rock":
         print("You won!")
         user_wins += 1
     elif user_input == "scissors" and computer_pick == "paper":
         print("You won!")
-        user_wins += 1
+        user_wins += 1 #the reason we use elif statements here is because using else statements would
+                        #require that we include another "continue" statement with each.
+                        #eliminating that reduces code volume 
 
     else: 
         print("You lost!")
-        computer_wins += 1
+        computer_wins += 1 #^since the above code accounts for all the cases our user can win
+                            #there is no need to account for all the loss cases, instead you can just increment computer_wins 
+                            #by one since these are the only parameters of the game 
 
 print("You won", user_wins, "times")
-print("The computer won", computer_wins, "times")
+print("The computer won", computer_wins, "times") #this info is printed upon quitting the game 
 print("goodbye")
 
